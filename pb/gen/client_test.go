b188bb4400fcdb37f8b226a1b757d0e2fa1fcf01
Nikki <suiguo3564@gmail.com>
2023-08-10 00:01:32 +0700

    修改测试数据

--- pb/gen/client_test.go
+++ pb/gen/client_test.go
@@ -7,7 +7,14 @@ import (
 )
 
 func TestClient(t *testing.T) {
-	cli := NewClient("3.39.236.151:8010", "testapi", []byte(TestPri), "Test")
-	resp, err := cli.CreateChainAccount(context.Background(), &CreateAccountRequest{ApiKey: "testkey", AccountId: "testuid"})
+	cli := NewClient("16.162.237.77:8010", "testapi", []byte(TestPri), "Test")
+	resp, err := cli.SendToken(context.Background(), &SendTokenRequest{
+		ApiKey:    "Test",
+		AccountId: "01bccb93-4e5f-4b5f-8c60-123dd62aa43f",
+		ToAddress: "0x0992ee761a4f7b8FC5a5ACAD011a2b6FdC9e8D72",
+		Amount:    "100",
+		Chain:     ChainType_BSC,
+	})
 	fmt.Println(resp, err)
+	// fmt.Println(resp, err)
 }
